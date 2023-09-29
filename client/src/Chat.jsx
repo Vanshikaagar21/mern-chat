cd1f46737022ec42af2d002783ff988f477b9658
Vanshika <vanshika.agar@gmail.com>
2023-09-29 12:47:59 +0530

    refactor display messages

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -47,7 +47,9 @@ export default function Chat() {
     if ("online" in messageData) {
       showOnlinePeople(messageData.online);
     } else if ("text" in messageData) {
-      setMessages((prev) => [...prev, { ...messageData }]);
+      if (messageData.sender === selectedUserId) {
+        setMessages((prev) => [...prev, { ...messageData }]);
+      }
     }
   }
 
