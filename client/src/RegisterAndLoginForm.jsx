ee21b3d1ea6d9313349be4a528d462abedcdbab2
Vanshika <vanshika.agar@gmail.com>
2023-09-29 12:48:16 +0530

    UI login shown first

--- client/src/RegisterAndLoginForm.jsx
+++ client/src/RegisterAndLoginForm.jsx
@@ -5,7 +5,7 @@ import { UserContext } from "./UserContext";
 export default function RegisterAndLoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoginOrRegister, setIsLoginOrRegister] = useState("register");
+  const [isLoginOrRegister, setIsLoginOrRegister] = useState("login");
 
   const { setUsername: setLoggedInUsername, setId } = useContext(UserContext);
 
@@ -41,16 +41,22 @@ export default function RegisterAndLoginForm() {
           {isLoginOrRegister === "register" && (
             <div>
               Already a member?
-              <button onClick={() => setIsLoginOrRegister("login")}>
-                Login here
+              <button
+                className="ml-1"
+                onClick={() => setIsLoginOrRegister("login")}
+              >
+                Login
               </button>
             </div>
           )}
           {isLoginOrRegister === "login" && (
             <div>
               Don't have an account?
-              <button onClick={() => setIsLoginOrRegister("register")}>
-                Register here
+              <button
+                className="ml-1"
+                onClick={() => setIsLoginOrRegister("register")}
+              >
+                Register
               </button>
             </div>
           )}
